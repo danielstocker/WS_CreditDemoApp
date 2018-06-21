50d6bff10dc085b642a2587c0aeedb11a3107f42
Daniel Stocker <dasto@microsoft.com>
2018-06-21 07:13:32 +0100

    app checkin

---
+++ CreditDemoWeb/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreditDemoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Our company is all about credit.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "This is our contact page. Please get in touch.";
+
+            return View();
+        }
+    }
+}
